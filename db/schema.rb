efad4b13847ca381ef93a0713894cf2e764297e2
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-09-22 12:59:33 +0900

    remove column email from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922033802) do
+ActiveRecord::Schema.define(version: 20170922035152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20170922033802) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -39,7 +38,6 @@ ActiveRecord::Schema.define(version: 20170922033802) do
     t.string   "provider",               default: "", null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
