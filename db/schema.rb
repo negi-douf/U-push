9af3d8cb6e3d3e7fb1d21d1f82477858811f489e
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-09-26 12:57:54 +0900

    change provider to name in users_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922035152) do
+ActiveRecord::Schema.define(version: 20170926035426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170922035152) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "uid",                    default: "", null: false
-    t.string   "provider",               default: "", null: false
+    t.string   "name",                   default: "", null: false
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
