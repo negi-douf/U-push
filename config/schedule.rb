64530773659691a7f9aa7f5d4d270d60bb255b4d
Hirata Koya <hiratakoya@Fione-Apple.local>
2017-11-14 21:34:19 +0900

    fix schedule to production

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,9 +4,9 @@
 # http://en.wikipedia.org/wiki/Cron
 
 set :output, "log/cron_log.log"
-set :environment, :development
+set :environment, :production
 
-every 1.day, at: "21:30" do
+every 1.day, at: "12:00" do
   # Rails 内のメソッド実行例
   runner "PagesController.check"
 end
